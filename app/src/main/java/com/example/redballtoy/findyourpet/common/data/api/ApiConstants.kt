234619f528998409e7a187e6db10b019d54a6e25
Dmitry Sorokin <redballtoy@gmail.com>
2022-10-13 19:07:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/redballtoy/findyourpet/common/data/api/ApiConstants.kt
@@ -0,0 +1,11 @@
+package com.example.redballtoy.findyourpet.common.data.api
+
+object ApiConstants {
+    const val BASE_ENDPOINT = "https://api.petfinder.com/v2/"
+    const val AUTH_ENDPOINT = "oauth/token/"
+    const val ANIMALS_ENDPOINT = "animals"
+
+    const val KEY ="v5FVO2bLCULV26GXAr3nx58FyelnUJMEEK6esufNQ1Zs92ULyI"
+    const val SECRET = "66JqjSkJ4cNko9BWRHVjil0pNYAjHxDpuUuB13VR"
+
+}
